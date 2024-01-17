0b9b3a69a69be89fb4ae1ff169751a23c2b02800
Rafael Silva Santos <rafael.silva.sts@gmail.com>
2024-01-16 21:07:19 -0300

    Fix read-me file.

---
+++ build/CMakeFiles/ufs.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/rafael/Workspace/GitHub/unsupervised-feature-selection/src/feature.cpp" "CMakeFiles/ufs.dir/src/feature.cpp.o" "gcc" "CMakeFiles/ufs.dir/src/feature.cpp.o.d"
+  "/home/rafael/Workspace/GitHub/unsupervised-feature-selection/src/featureio.cpp" "CMakeFiles/ufs.dir/src/featureio.cpp.o" "gcc" "CMakeFiles/ufs.dir/src/featureio.cpp.o.d"
+  "/home/rafael/Workspace/GitHub/unsupervised-feature-selection/src/main.cpp" "CMakeFiles/ufs.dir/src/main.cpp.o" "gcc" "CMakeFiles/ufs.dir/src/main.cpp.o.d"
+  "/home/rafael/Workspace/GitHub/unsupervised-feature-selection/src/measure.cpp" "CMakeFiles/ufs.dir/src/measure.cpp.o" "gcc" "CMakeFiles/ufs.dir/src/measure.cpp.o.d"
+  "/home/rafael/Workspace/GitHub/unsupervised-feature-selection/src/similaritymat.cpp" "CMakeFiles/ufs.dir/src/similaritymat.cpp.o" "gcc" "CMakeFiles/ufs.dir/src/similaritymat.cpp.o.d"
+  "/home/rafael/Workspace/GitHub/unsupervised-feature-selection/src/ufs.cpp" "CMakeFiles/ufs.dir/src/ufs.cpp.o" "gcc" "CMakeFiles/ufs.dir/src/ufs.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
